9ac6058a7923bfa75bfcd2a8153ecb0e60bfcf69
matchbean <1224231492@qq.com>
2023-04-04 01:50:49 +0800

    feat : 布局优化

--- src/App.vue
+++ src/App.vue
@@ -40,9 +40,15 @@
             tag: 'Office',
         },
     ])
+    let multipleSelection = ref([])
 
     const handleRowClick = () => {
         console.log('click')
+    }
+
+    const handleSelectionChange = (val) => {
+        multipleSelection.value = val
+        console.log(val);
     }
 </script>
 
@@ -57,7 +63,15 @@
             <el-button type="primary">增加</el-button>
         </div>
 
-        <el-table :data="tableData" style="width: 100%">
+        <el-table
+            border
+            ref="multipleTableRef"
+            :data="tableData"
+            @selection-change="handleSelectionChange"
+            style="width: 100%">
+
+            <el-table-column type="selection" width="55" />
+
             <el-table-column fixed prop="date" label="Date" width="150" />
             <el-table-column prop="name" label="Name" width="120" />
             <el-table-column prop="state" label="State" width="120" />
@@ -83,5 +97,16 @@
     top: 50%;
     left: 50%;
     transform: translate(-50%,-50%);
+}
+.title{
+    text-align: center;
+}
+.query-box{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+}
+.el-input{
+    width: 200px;
 }
 </style>
